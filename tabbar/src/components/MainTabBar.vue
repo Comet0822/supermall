2e444aa5297a16c1d3193c7a4dd852ef458a54dd
comet0822 <1724274377@qq.com>
2021-08-10 23:54:57 +0800

    naster

---
+++ tabbar/src/components/MainTabBar.vue
@@ -0,0 +1,41 @@
+<template>
+   <tab-bar>
+      <tab-bar-item path="/home" activeColor="skyblue">
+        <img slot="item-icon" src="../assets/img/tabbar/kucun.png" alt="">
+         <img slot="item-icon-active" src="../assets/img/tabbar/kucunred.png" alt="">
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item path="/cart" activeColor="skyblue">
+        <img slot="item-icon" src="../assets/img/tabbar/cart.png" alt="">
+         <img slot="item-icon-active" src="../assets/img/tabbar/cartred.png" alt="">
+        <div slot="item-text">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item path="/category" activeColor="skyblue">
+        <img slot="item-icon" src="../assets/img/tabbar/renew.png" alt="">
+         <img slot="item-icon-active" src="../assets/img/tabbar/renewred.png" alt="">
+        <div slot="item-text">分类</div>
+      </tab-bar-item>
+      <tab-bar-item path="/profile" activeColor="skyblue"> 
+        <img slot="item-icon" src="../assets/img/tabbar/person.png" alt="">
+         <img slot="item-icon-active" src="../assets/img/tabbar/personred.png" alt="">
+        <div slot="item-text">我的</div>
+      </tab-bar-item>
+    </tab-bar>
+</template>
+
+<script>
+import TabBar from "./tabbar/TabBar";
+import TabBarItem from "./tabbar/TabBarItem";
+export default {
+    name:'MainTabBar',
+  components: {
+    TabBar,
+    TabBarItem
+  }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ testvuecil3/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <!-- <router-link to="/home" tag="button">首页 </router-link>
+      <router-link to="/about" tag="button">关于</router-link> -->
+      <button @click="linktohome">首页</button>
+      <button @click="linktoabout">关于</button>
+    </div>
+      <router-view/>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name:'App',
+  methods :{
+      linktohome(){
+        this.$router.push('/home')
+      },
+      linktoabout(){
+         this.$router.push('/about')
+      }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
